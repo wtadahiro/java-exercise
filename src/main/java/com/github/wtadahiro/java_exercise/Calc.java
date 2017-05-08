864e23332a273adb0ae5df37e9a0ce022d95b8dc
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-05-08 18:07:34 +0900

    add cases

--- src/main/java/com/github/wtadahiro/java_exercise/Calc.java
+++ src/main/java/com/github/wtadahiro/java_exercise/Calc.java
@@ -1,7 +1,11 @@
-package com.wtadahiro;
+package com.github.wtadahiro.java_exercise;
 
 public class Calc {
   public static int add(int a, int b) {
     return a + b;
+  }
+
+  public static int subtract(int a, int b) {
+    return a - b;
   }
 }
--- src/test/java/com/github/wtadahiro/java_exercise/CalcTest.java
+++ src/test/java/com/github/wtadahiro/java_exercise/CalcTest.java
@@ -1,4 +1,4 @@
-package com.wtadahiro;
+package com.github.wtadahiro.java_exercise;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -8,5 +8,10 @@ public class CalcTest {
   @Test
   public void testAdd() {
     assertEquals(3, Calc.add(1, 2));
+  }
+
+  @Test
+  public void testSubtract() {
+    assertEquals(3, Calc.subtract(8, 5));
   }
 }
