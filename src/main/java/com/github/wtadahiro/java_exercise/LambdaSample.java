d97808d3912b185cfc739499b79d6e16005b20e3
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-05-09 19:14:49 +0900

    Lambda sample

--- src/main/java/com/github/wtadahiro/java_exercise/LambdaSample.java
+++ src/main/java/com/github/wtadahiro/java_exercise/LambdaSample.java
@@ -1,4 +1,19 @@
 package com.github.wtadahiro.java_exercise;
 
-public class LambdaSample  {
+import java.util.List;
+import java.util.ArrayList;
+
+public class LambdaSample {
+
+  public static int sumUsingLoop(List<Integer> arrayList) {
+    int sum = 0;
+    for(int i = 0; i< arrayList.size(); i++) {
+      sum += arrayList.get(i);
+    }
+    return sum;
+  }
+
+  public static int sumUsingLambda(List<Integer> arrayList) {
+    return arrayList.stream().reduce(0, (prev, current) -> prev + current);
+  }
 }
--- src/test/java/com/github/wtadahiro/java_exercise/LambdaSampleTest.java
+++ src/test/java/com/github/wtadahiro/java_exercise/LambdaSampleTest.java
@@ -2,6 +2,23 @@ package com.github.wtadahiro.java_exercise;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class LambdaSampleTest {
+
+  @Test
+  public void test() {
+    List list = new ArrayList<Integer>();
+    list.add(1);
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(5);
+    list.add(8);
+    assertEquals(LambdaSample.sumUsingLoop(list), LambdaSample.sumUsingLambda(list));
+  }
+
+
+
 }
